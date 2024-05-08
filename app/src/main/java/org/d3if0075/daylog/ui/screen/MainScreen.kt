57ba266e9ebaeaa4836a032cb732e3dc450c97e7
Fretty Simamora <purpieshomie@gmail.com>
2024-05-08 09:20:18 +0700

    Membuat halaman beranda

--- app/src/main/java/org/d3if0075/daylog/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0075/daylog/ui/screen/MainScreen.kt
@@ -1,38 +1,24 @@
 package org.d3if0075.daylog.ui.screen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import org.d3if0075.daylog.R
-import org.d3if0075.daylog.model.loadImage
 import org.d3if0075.daylog.ui.theme.DayLogTheme
 @Composable
 fun MainScreen(navHostController: NavHostController) {
-    val backgroundImage = loadImage(R.drawable.background_daylog)
-
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        Image(
-            bitmap = backgroundImage,
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.FillBounds
-        )
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -41,20 +27,10 @@ fun MainScreen(navHostController: NavHostController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = stringResource(R.string.app_name),
-                fontSize = 36.sp,
-                fontStyle = FontStyle.Italic,
+                text = stringResource(R.string.opening),
+                fontSize = 16.sp,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
-            Text(
-                text = stringResource(R.string.selamat_datang),
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            MulaiButton(onClick = {
-                // Action when the button is clicked, e.g., navigate to another screen
-            })
         }
     }
 }
